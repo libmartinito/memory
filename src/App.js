8e8194e16c5d960083bbba7ec1c1344ef8ee8566
lib <lib.martinito@pm.me>
2022-03-10 17:38:26 +0800

    Added modal for when the game ends as well as some styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,25 @@ import React, { useState } from 'react'
 import './App.css'
 import { v4 as uuidv4 } from 'uuid'
 
+const EndModal = ({ hideModal, bestScore, isFinished }) => {
+  if(isFinished) {
+    return(
+      <div className = 'end-modal'>
+        <div className = 'end-modal__content'>
+          <div className = 'end-modal__close-wrapper'>
+            <div className = 'end-modal__close' onClick = {hideModal}>X</div>
+          </div>
+          <div className = 'end-modal__body-one'>"We are all the pieces of what we remember. We hold in ourselves the hopes and fears of those who love us. As long as there is love and memory, there is no true loss."</div>
+          <div className = 'end-modal__body-two'>{bestScore}</div>
+          <div className = 'end-modal__body-three'>This experience as well as the points are now a part of you. Do with it what you will.</div>
+        </div>
+      </div>
+    )
+  } else {
+    return(null)
+  }
+}
+
 const App = () => {
   const [score, setScore] = useState(0)
   const [bestScore, setBestScore] = useState(0)
@@ -30,6 +49,7 @@ const App = () => {
     './images/the-world.svg'
   ])
   const [cards, setCards] = useState([])
+  const [isFinished, setIsFinished] = useState(false)
 
   const incrementScore = () => {
     setScore(score + 1)
@@ -52,6 +72,14 @@ const App = () => {
     setCards([])
   }
 
+  const displayModal = () => {
+    setIsFinished(true)
+  }
+
+  const hideModal = () => {
+    setIsFinished(false)
+  }
+
   const randomizeImages = (arr) => {
     const imageArr = arr
     for(let i = imageArr.length - 1; i > 0; i--) {
@@ -68,6 +96,7 @@ const App = () => {
       updateBestScore()
       resetScore()
       resetCards()
+      displayModal()
     } else {
       incrementScore()
       updateCards(e)
@@ -100,6 +129,11 @@ const App = () => {
           ></img>  
         )}
       </div>
+      <EndModal
+        hideModal = {hideModal}
+        bestScore = {bestScore}
+        isFinished = {isFinished}
+      />
     </div>
   )
 }
