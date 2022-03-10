9d3bc858750faa85b4ce4721b2b22c95d8f5a964
lib <lib.martinito@pm.me>
2022-03-10 18:52:49 +0800

    Updates

---
+++ precache-manifest.d07ec7e0d84a9ae6a8dc13f182a8476a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d08f89c4f340b4fa820c",
+    "url": "/memory/static/css/main.79de0dcf.chunk.css"
+  },
+  {
+    "revision": "d08f89c4f340b4fa820c",
+    "url": "/memory/static/js/main.47eaab26.chunk.js"
+  },
+  {
+    "revision": "174810366761c26401f3",
+    "url": "/memory/static/js/runtime~main.031dcd1e.js"
+  },
+  {
+    "revision": "9de343108938919dac1d",
+    "url": "/memory/static/js/2.2f9bb584.chunk.js"
+  },
+  {
+    "revision": "a4bdd6d0e9056f5346ef221d860712f9",
+    "url": "/memory/index.html"
+  }
+];
